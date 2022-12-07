37291d5b69670cdcb97f83fb41e872a3cf3455dc
Kevin Kurniadi <kurniadikevin@gmail.com>
2022-12-07 23:20:21 +0700

    input asset for testimonial

---
+++ src/component/main/testimonial/testimonial.js
@@ -0,0 +1,21 @@
+const Testimonial = ()=>{
+
+    const testimonialArr= [{
+        user : 'Floyd Miles', text : `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+        Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`
+     }, {user:'Ronald Richards', text: `ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`},
+    {user : `Savannah Nguyen`, text : `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+    Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`},
+    {user : 'Floyd Miles', text : `Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.`}];
+
+    return (
+        <div className="testimonial">
+            <div className="testimoail-head">
+                <div>Testimonial</div>
+                <div>A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  standard dummy text </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonial;
